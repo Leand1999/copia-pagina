97d2300d0b419550ee5c6b85e9efdff133745f17
Leand1999 <143460386+Leand1999@users.noreply.github.com>
2024-09-05 03:34:44 -0300

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,15 @@
-let dolarHoy = 3000
-
-function calcDolar(hola, dos, tres, cuatro, hola2 ){
-    let cantDolar= cant / 
-
+const pi=3.14
+function calcular_area_circulo(radio){
+    let circulo= pi * radio**2
+    console.log(circulo);
 }
+
+calcular_area_circulo(4)
+console.log
+
+function CalcularDole(num){
+    let sum= num + 2;
+    return 200
+}
+
+console.log (CalcularDole(2) );
